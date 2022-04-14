3259c36bc12154cf16cd0dd32795edea43ee3b92
steladalecio <95080136+steladalecio@users.noreply.github.com>
2022-04-14 19:19:43 -0400

    first commit

---
+++ src/main/java/br/edu/ifms/crudspring/CrudSpringApplication.java
@@ -0,0 +1,21 @@
+package br.edu.ifms.crudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@SpringBootApplication
+@RestController
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+	@GetMapping("/")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("telaInicial");
+		return mv;
+	}
+}
---
+++ src/main/java/br/edu/ifms/crudspring/model/Student.java
@@ -0,0 +1,5 @@
+package br.edu.ifms.crudspring.model;
+
+public class Student {
+    
+}
